b23074ec34918b862105b5e4fa687dbb0cbd6531
AkmalxonWeBd <akmalxonizzatullayev770@gmail.com>
2024-04-03 20:14:57 +0500

    salom

--- src/components/pages/layout.jsx
+++ src/components/pages/layout.jsx
@@ -1,54 +1,50 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import { FaHome } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { MdAssessment } from "react-icons/md";
 import { MdDashboard } from "react-icons/md";
 import { MdGroups2 } from "react-icons/md";
 import "../sidebar.css";
+
 const Layout = () => {
   return (
     <div className="container">
       <nav className="sidebar">
         <ul className="icon">
-         
-            <Link to="/Home">
-                <li className="active">
-              <FaHome />
-              Home{" "}
-              </li>
-            </Link>
-          
-          
-            <Link to="/Mail">
-             <li>
-             <IoIosMail />
-              Mail
-             </li>
-            </Link>
-          
-          
-            <Link to="/Analystics">
-              <li><MdAssessment />
-              Analystics</li>
-            </Link>
-          
-         
-            <Link to="/Dashboard">
-             <li>
-             <MdDashboard />
-              Dashboard
-             </li>
-           
-            </Link>
-          
-        
-            <Link to="/Friends">
-              <li>
-              <MdGroups2 />
-              Friends
-              </li>
-            </Link>
-          
+          <NavLink to="/Home" activeClassName="active">
+            <li className="sidebarmenu">
+              <FaHome className="img" />
+              <h1>Home</h1>
+            </li>
+          </NavLink>
+
+          <NavLink to="/Mail" activeClassName="active">
+            <li className="sidebarmenu">
+              <IoIosMail className="img" />
+              <h1>Mail</h1>
+            </li>
+          </NavLink>
+
+          <NavLink to="/Analystics" activeClassName="active">
+            <li className="sidebarmenu">
+              <MdAssessment className="img" />
+              <h1>Analystics</h1>
+            </li>
+          </NavLink>
+
+          <NavLink to="/Dashboard" activeClassName="active">
+            <li className="sidebarmenu">
+              <MdDashboard className="img" />
+              <h1>Dashboard</h1>
+            </li>
+          </NavLink>
+
+          <NavLink to="/Friends" activeClassName="active">
+            <li className="sidebarmenu">
+              <MdGroups2 className="img" />
+              <h1>Friends</h1>
+            </li>
+          </NavLink>
         </ul>
       </nav>
       <div className="sidebarcom">
